86578ee676e328006852bb7c1c1b6558d78d7e2b
D054906 <D054906@W-PF2XDCR9>
2022-07-26 10:31:20 +0200

    testing

---
+++ wwibe221/src/helpers/SimpleCalculator.java
@@ -0,0 +1,24 @@
+package helpers;
+
+/**
+ * Einfacher Taschenrechner
+ * 
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class SimpleCalculator {
+
+	public int abs(int a) {
+		return 0;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return 0;
+	}
+
+}
