925b369d601213132b1b33a67244d6505ba23482
D054906 <D054906@W-PF2XDCR9>
2022-07-28 10:32:20 +0200

    testing

--- wwibe221/src/tests/TableLampStoreTest.java
+++ wwibe221/src/tests/TableLampStoreTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.awt.Color;
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
