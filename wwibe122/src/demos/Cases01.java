67f64630efc2e09a5e8676c4c8a8cab8e4224570
D054906 <D054906@W-PF2XDCR9>
2022-11-08 09:15:11 +0100

    cases

---
+++ wwibe122/src/demos/Cases01.java
@@ -0,0 +1,48 @@
+package demos;
+
+import java.util.Scanner;
+
+/**
+ * Verzweigungen
+ * 
+ * @author Daniel Appenmaier
+ *
+ */
+public class Cases01 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int age;
+
+    System.out.print("Bitte gib Dein Alter ein: ");
+    age = sc.nextInt();
+
+    /* Einfachverzweigung */
+    if (age >= 18) {
+      System.out.println("Du bist volljährig");
+    } else {
+      System.out.println("Du bist minderjährig");
+    }
+
+    /*
+     * Vergleichsoperatoren: != (ungleich), == (gleich), > (größer), >= (größergleich), < (kleiner),
+     * <= (kleinergleich)
+     */
+    /*
+     * Logische Operatoren: && (logisches Und), || (logisches Oder), ! (logische Verneinung)
+     */
+
+    /* Verschachtelte Verzweigungen */
+    if (age < 12) {
+      System.out.println("Du bist ein Kind");
+    } else if (age < 18) {
+      System.out.println("Du bist ein:e Jugendliche:r");
+    } else {
+      System.out.println("Du bist ein:e Erwachsene:r");
+    }
+
+  }
+
+}
