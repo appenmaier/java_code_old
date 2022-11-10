8bb4c10d9b6285683044b29bb08538caff95d71f
D054906 <D054906@W-PF2XDCR9>
2022-11-10 09:41:52 +0100

    loops

--- wwibe122/src/cashiersystem/MainClass.java
+++ wwibe122/src/cashiersystem/MainClass.java
@@ -14,38 +14,48 @@ public class MainClass {
     @SuppressWarnings("resource")
     Scanner sc = new Scanner(System.in);
 
-    String nameOfArticle1, nameOfArticle2, nameOfArticle3;
-    int amountOfArticle1, amountOfArticle2, amountOfArticle3;
-    double priceOfArticle1, priceOfArticle2, priceOfArticle3;
+    String nameOfArticle1, nameOfArticle2 = "", nameOfArticle3 = "";
+    int amountOfArticle1, amountOfArticle2 = 0, amountOfArticle3 = 0;
+    double priceOfArticle1, priceOfArticle2 = 0, priceOfArticle3 = 0;
+    double totalPriceOfArticle1, totalPriceOfArticle2 = 0, totalPriceOfArticle3 = 0;
+    double totalPrice;
+
+    System.out.print("Wie viele Artikel moechtest Du eingeben?: ");
+    int numberOfArticles = sc.nextInt();
 
     System.out.print("Artikelbezeichner (Artikel 1): ");
+    sc.nextLine();
     nameOfArticle1 = sc.nextLine();
-    System.out.print("Artikelbezeichner (Artikel 2): ");
-    nameOfArticle2 = sc.nextLine();
-    System.out.print("Artikelbezeichner (Artikel 3): ");
-    nameOfArticle3 = sc.nextLine();
-
     System.out.print("Menge (Artikel 1): ");
     amountOfArticle1 = sc.nextInt();
-    System.out.print("Menge (Artikel 2): ");
-    amountOfArticle2 = sc.nextInt();
-    System.out.print("Menge (Artikel 3): ");
-    amountOfArticle3 = sc.nextInt();
-
     System.out.print("Peis (Artikel 1): ");
     priceOfArticle1 = sc.nextDouble();
-    System.out.print("Peis (Artikel 2): ");
-    priceOfArticle2 = sc.nextDouble();
-    System.out.print("Peis (Artikel 3): ");
-    priceOfArticle3 = sc.nextDouble();
-
-    double totalPriceOfArticle1, totalPriceOfArticle2, totalPriceOfArticle3;
-
     totalPriceOfArticle1 = priceOfArticle1 * amountOfArticle1;
-    totalPriceOfArticle2 = priceOfArticle2 * amountOfArticle2;
-    totalPriceOfArticle3 = priceOfArticle3 * amountOfArticle3;
+    totalPrice = totalPriceOfArticle1;
 
-    double totalPrice = totalPriceOfArticle1 + totalPriceOfArticle2 + totalPriceOfArticle3;
+    if (numberOfArticles > 1) {
+      System.out.print("Artikelbezeichner (Artikel 2): ");
+      sc.nextLine();
+      nameOfArticle2 = sc.nextLine();
+      System.out.print("Menge (Artikel 2): ");
+      amountOfArticle2 = sc.nextInt();
+      System.out.print("Peis (Artikel 2): ");
+      priceOfArticle2 = sc.nextDouble();
+      totalPriceOfArticle2 = priceOfArticle2 * amountOfArticle2;
+      totalPrice += totalPriceOfArticle2;
+    }
+
+    if (numberOfArticles > 2) {
+      System.out.print("Artikelbezeichner (Artikel 3): ");
+      sc.nextLine();
+      nameOfArticle3 = sc.nextLine();
+      System.out.print("Menge (Artikel 3): ");
+      amountOfArticle3 = sc.nextInt();
+      System.out.print("Peis (Artikel 3): ");
+      priceOfArticle3 = sc.nextDouble();
+      totalPriceOfArticle3 = priceOfArticle3 * amountOfArticle3;
+      totalPrice += totalPriceOfArticle3;
+    }
 
     /* Bon */
     System.out.println("Tante-Emma-Laden");
@@ -54,10 +64,14 @@ public class MainClass {
     System.out.println("Artikel:");
     System.out.println(nameOfArticle1 + ", " + amountOfArticle1 + ", " + priceOfArticle1 + ", "
         + totalPriceOfArticle1);
-    System.out.println(nameOfArticle2 + ", " + amountOfArticle2 + ", " + priceOfArticle2 + ", "
-        + totalPriceOfArticle2);
-    System.out.println(nameOfArticle3 + ", " + amountOfArticle3 + ", " + priceOfArticle3 + ", "
-        + totalPriceOfArticle3);
+    if (numberOfArticles > 1) {
+      System.out.println(nameOfArticle2 + ", " + amountOfArticle2 + ", " + priceOfArticle2 + ", "
+          + totalPriceOfArticle2);
+    }
+    if (numberOfArticles > 2) {
+      System.out.println(nameOfArticle3 + ", " + amountOfArticle3 + ", " + priceOfArticle3 + ", "
+          + totalPriceOfArticle3);
+    }
     System.out.println("-----------------------------------------");
     System.out.println("Gesamtbetrag: " + totalPrice);
 
---
+++ wwibe122/src/exercises/java/Loops03.java
@@ -0,0 +1,38 @@
+package exercises.java;
+
+import java.util.Scanner;
+
+public class Loops03 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Gib bitte eine Zeichenkette ein: ");
+    String text = sc.nextLine();
+
+    System.out.print("Gib bitte das zu analysierende Zeichen ein: ");
+    char token = sc.next().charAt(0);
+
+    int count = 0;
+    // for (int i = 0; i < text.length(); i++) {
+    // if (text.charAt(i) == token) {
+    // count++;
+    // }
+    // }
+    int i = 0;
+    while (i < text.length()) {
+      if (text.charAt(i) == token) {
+        count++;
+      }
+      i++;
+    }
+
+    double percentage = count * 100.0 / text.length();
+
+    System.out.printf("Absoluter Anteil: %d%n", count);
+    System.out.printf("Prozentualer Anteil: %.2f%%", percentage);
+
+  }
+
+}
