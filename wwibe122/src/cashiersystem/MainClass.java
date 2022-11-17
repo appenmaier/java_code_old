93711f9afa25c69e2d1de1012a73ca6d051ca7a1
D054906 <D054906@W-PF2XDCR9>
2022-11-17 09:57:48 +0100

    exercises

--- wwibe122/src/cashiersystem/MainClass.java
+++ wwibe122/src/cashiersystem/MainClass.java
@@ -14,47 +14,30 @@ public class MainClass {
     @SuppressWarnings("resource")
     Scanner sc = new Scanner(System.in);
 
-    String nameOfArticle1, nameOfArticle2 = "", nameOfArticle3 = "";
-    int amountOfArticle1, amountOfArticle2 = 0, amountOfArticle3 = 0;
-    double priceOfArticle1, priceOfArticle2 = 0, priceOfArticle3 = 0;
-    double totalPriceOfArticle1, totalPriceOfArticle2 = 0, totalPriceOfArticle3 = 0;
-    double totalPrice;
+    String[] namesOfArticles;
+    int[] amountsOfArticles;
+    double[] pricesOfArticles;
+    double[] totalPricesOfArticles;
+    double totalPrice = 0.0;
 
     System.out.print("Wie viele Artikel moechtest Du eingeben?: ");
     int numberOfArticles = sc.nextInt();
 
-    System.out.print("Artikelbezeichner (Artikel 1): ");
-    sc.nextLine();
-    nameOfArticle1 = sc.nextLine();
-    System.out.print("Menge (Artikel 1): ");
-    amountOfArticle1 = sc.nextInt();
-    System.out.print("Peis (Artikel 1): ");
-    priceOfArticle1 = sc.nextDouble();
-    totalPriceOfArticle1 = priceOfArticle1 * amountOfArticle1;
-    totalPrice = totalPriceOfArticle1;
+    namesOfArticles = new String[numberOfArticles];
+    amountsOfArticles = new int[numberOfArticles];
+    pricesOfArticles = new double[numberOfArticles];
+    totalPricesOfArticles = new double[numberOfArticles];
 
-    if (numberOfArticles > 1) {
-      System.out.print("Artikelbezeichner (Artikel 2): ");
+    for (int i = 0; i < numberOfArticles; i++) {
+      System.out.print("Artikel: ");
       sc.nextLine();
-      nameOfArticle2 = sc.nextLine();
-      System.out.print("Menge (Artikel 2): ");
-      amountOfArticle2 = sc.nextInt();
-      System.out.print("Peis (Artikel 2): ");
-      priceOfArticle2 = sc.nextDouble();
-      totalPriceOfArticle2 = priceOfArticle2 * amountOfArticle2;
-      totalPrice += totalPriceOfArticle2;
-    }
-
-    if (numberOfArticles > 2) {
-      System.out.print("Artikelbezeichner (Artikel 3): ");
-      sc.nextLine();
-      nameOfArticle3 = sc.nextLine();
-      System.out.print("Menge (Artikel 3): ");
-      amountOfArticle3 = sc.nextInt();
-      System.out.print("Peis (Artikel 3): ");
-      priceOfArticle3 = sc.nextDouble();
-      totalPriceOfArticle3 = priceOfArticle3 * amountOfArticle3;
-      totalPrice += totalPriceOfArticle3;
+      namesOfArticles[i] = sc.nextLine();
+      System.out.print("Menge: ");
+      amountsOfArticles[i] = sc.nextInt();
+      System.out.print("Peis: ");
+      pricesOfArticles[i] = sc.nextDouble();
+      totalPricesOfArticles[i] = pricesOfArticles[i] * amountsOfArticles[i];
+      totalPrice += totalPricesOfArticles[i];
     }
 
     /* Bon */
@@ -62,15 +45,9 @@ public class MainClass {
     System.out.println("03.11.2022");
     System.out.println();
     System.out.println("Artikel:");
-    System.out.println(nameOfArticle1 + ", " + amountOfArticle1 + ", " + priceOfArticle1 + ", "
-        + totalPriceOfArticle1);
-    if (numberOfArticles > 1) {
-      System.out.println(nameOfArticle2 + ", " + amountOfArticle2 + ", " + priceOfArticle2 + ", "
-          + totalPriceOfArticle2);
-    }
-    if (numberOfArticles > 2) {
-      System.out.println(nameOfArticle3 + ", " + amountOfArticle3 + ", " + priceOfArticle3 + ", "
-          + totalPriceOfArticle3);
+    for (int i = 0; i < numberOfArticles; i++) {
+      System.out.println(i + ": " + namesOfArticles[i] + ", " + amountsOfArticles[i] + ", "
+          + pricesOfArticles[i] + ", " + totalPricesOfArticles[i]);
     }
     System.out.println("-----------------------------------------");
     System.out.println("Gesamtbetrag: " + totalPrice);
---
+++ wwibe122/src/exercises/java/Arrays04.java
@@ -0,0 +1,52 @@
+package exercises.java;
+
+public class Arrays04 {
+
+  public static void main(String[] args) {
+
+    int[] binaryNumber1 = {1, 0, 0, 1, 0, 1, 1, 0};
+    int[] binaryNumber2 = {0, 1, 1, 0, 0, 1, 0, 1};
+    int[] binaryNumber3 = new int[8];
+
+    int carry = 0;
+
+    for (int i = binaryNumber1.length - 1; i >= 0; i--) {
+      int subTotal = binaryNumber1[i] + binaryNumber2[i] + carry;
+
+      if (subTotal == 0 || subTotal == 1) {
+        binaryNumber3[i] = subTotal;
+        carry = 0;
+      } else if (subTotal == 2) {
+        binaryNumber3[i] = 0;
+        carry = 1;
+      } else {
+        binaryNumber3[i] = 1;
+        carry = 1;
+      }
+    }
+
+    if (carry == 1) {
+      System.err.println("Ueberlauf");
+      return;
+    }
+
+    System.out.print("  ");
+    for (int i = 0; i < binaryNumber1.length; i++) {
+      System.out.print(binaryNumber1[i]);
+    }
+    System.out.println();
+
+    System.out.print("+ ");
+    for (int i = 0; i < binaryNumber2.length; i++) {
+      System.out.print(binaryNumber2[i]);
+    }
+    System.out.println();
+
+    System.out.print("= ");
+    for (int i = 0; i < binaryNumber3.length; i++) {
+      System.out.print(binaryNumber3[i]);
+    }
+
+  }
+
+}
