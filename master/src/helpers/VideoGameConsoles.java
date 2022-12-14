3c2b995e3868fa8c417399d437272226eab30d0c
D054906 <D054906@W-PF2XDCR9>
2022-12-14 13:36:28 +0100

    exams

--- master/src/helpers/VideoGameConsoles.java
+++ master/src/helpers/VideoGameConsoles.java
@@ -4,48 +4,64 @@ import java.util.ArrayList;
 
 public class VideoGameConsoles {
 
-	private final static VideoGameConsole PS2 = new VideoGameConsole("PlayStation 2", Maker.SONY, 12, 158.70);
-	private final static VideoGameConsole DS = new VideoGameConsole("Nintendo DS", Maker.NINTENDO, 10, 154.02);
-	private final static VideoGameConsole GB = new VideoGameConsole("GameBoy", Maker.NINTENDO, 10, 118.69);
-	private final static VideoGameConsole PS4 = new VideoGameConsole("PlayStation 4", Maker.SONY, -1, 116.93);
-	private final static VideoGameConsole NS = new VideoGameConsole("Nintendo Switch", Maker.NINTENDO, -1, 109.25);
-	private final static VideoGameConsole PSX = new VideoGameConsole("PlayStation", Maker.SONY, 9, 102.49);
-	private final static VideoGameConsole WII = new VideoGameConsole("Nintendo Wii", Maker.NINTENDO, 13, 101.63);
-	private final static VideoGameConsole PS3 = new VideoGameConsole("PlayStation 3", Maker.SONY, 11, 87.40);
-	private final static VideoGameConsole X360 = new VideoGameConsole("XBox 360", Maker.MICROSOFT, 12, 85.81);
-	private final static VideoGameConsole GBA = new VideoGameConsole("GameBoy Advance", Maker.NINTENDO, 7, 81.51);
-	private final static VideoGameConsole PSP = new VideoGameConsole("PlayStation Portable", Maker.SONY, 9, 80.79);
-	private final static VideoGameConsole TDS = new VideoGameConsole("Nintendo 3DS", Maker.NINTENDO, 8, 75.94);
-	private final static VideoGameConsole NES = new VideoGameConsole("Nintendo Entertainment System", Maker.NINTENDO, 9, 61.91);
-	private final static VideoGameConsole XONE = new VideoGameConsole("XBox One", Maker.MICROSOFT, -1, 50.53);
-	private final static VideoGameConsole SNES = new VideoGameConsole("Super Nintendo Entertainment System", Maker.NINTENDO, 7, 49.10);
+  private final static VideoGameConsole PS2 =
+      new VideoGameConsole("PlayStation 2", Maker.SONY, 12, 158.70);
+  private final static VideoGameConsole DS =
+      new VideoGameConsole("Nintendo DS", Maker.NINTENDO, 10, 154.02);
+  private final static VideoGameConsole GB =
+      new VideoGameConsole("GameBoy", Maker.NINTENDO, 10, 118.69);
+  private final static VideoGameConsole PS4 =
+      new VideoGameConsole("PlayStation 4", Maker.SONY, -1, 116.93);
+  private final static VideoGameConsole NS =
+      new VideoGameConsole("Nintendo Switch", Maker.NINTENDO, -1, 109.25);
+  private final static VideoGameConsole PSX =
+      new VideoGameConsole("PlayStation", Maker.SONY, 9, 102.49);
+  private final static VideoGameConsole WII =
+      new VideoGameConsole("Nintendo Wii", Maker.NINTENDO, 13, 101.63);
+  private final static VideoGameConsole PS3 =
+      new VideoGameConsole("PlayStation 3", Maker.SONY, 11, 87.40);
+  private final static VideoGameConsole X360 =
+      new VideoGameConsole("XBox 360", Maker.MICROSOFT, 12, 85.81);
+  private final static VideoGameConsole GBA =
+      new VideoGameConsole("GameBoy Advance", Maker.NINTENDO, 7, 81.51);
+  private final static VideoGameConsole PSP =
+      new VideoGameConsole("PlayStation Portable", Maker.SONY, 9, 80.79);
+  private final static VideoGameConsole TDS =
+      new VideoGameConsole("Nintendo 3DS", Maker.NINTENDO, 8, 75.94);
+  private final static VideoGameConsole NES =
+      new VideoGameConsole("Nintendo Entertainment System", Maker.NINTENDO, 9, 61.91);
+  private final static VideoGameConsole XONE =
+      new VideoGameConsole("XBox One", Maker.MICROSOFT, -1, 50.53);
+  private final static VideoGameConsole SNES =
+      new VideoGameConsole("Super Nintendo Entertainment System", Maker.NINTENDO, 7, 49.10);
 
-	public static record VideoGameConsole(String title, Maker maker, int lifespan, double soldUnits) {
+  public static record VideoGameConsole(String title, Maker maker, int lifespan, double soldUnits) {
 
-	}
+  }
 
-	public static enum Maker {
-		NINTENDO, SEGA, SONY, MICROSOFT;
-	}
+  public static enum Maker {
+    NINTENDO, SEGA, SONY, MICROSOFT;
+  }
 
-	public static ArrayList<VideoGameConsole> getVideoGameConsoles() {
-		ArrayList<VideoGameConsole> videoGameConsoles = new ArrayList<>();
-		videoGameConsoles.add(PS2);
-		videoGameConsoles.add(DS);
-		videoGameConsoles.add(GB);
-		videoGameConsoles.add(PS4);
-		videoGameConsoles.add(NS);
-		videoGameConsoles.add(PSX);
-		videoGameConsoles.add(WII);
-		videoGameConsoles.add(PS3);
-		videoGameConsoles.add(X360);
-		videoGameConsoles.add(GBA);
-		videoGameConsoles.add(PSP);
-		videoGameConsoles.add(TDS);
-		videoGameConsoles.add(NES);
-		videoGameConsoles.add(XONE);
-		videoGameConsoles.add(SNES);
-		return videoGameConsoles;
-	}
+  public static ArrayList<VideoGameConsole> getVideoGameConsoles() {
+    ArrayList<VideoGameConsole> videoGameConsoles = new ArrayList<>();
+    videoGameConsoles.add(PS2);
+    videoGameConsoles.add(DS);
+    videoGameConsoles.add(GB);
+    videoGameConsoles.add(PS4);
+    videoGameConsoles.add(NS);
+    videoGameConsoles.add(PSX);
+    videoGameConsoles.add(WII);
+    videoGameConsoles.add(PS3);
+    videoGameConsoles.add(X360);
+    videoGameConsoles.add(GBA);
+    videoGameConsoles.add(PSP);
+    videoGameConsoles.add(TDS);
+    videoGameConsoles.add(NES);
+    videoGameConsoles.add(XONE);
+    videoGameConsoles.add(SNES);
+    return videoGameConsoles;
+  }
 
 }
+
---
+++ wwibe122/src/exams/exam2022q4/task02/Present.java
@@ -0,0 +1,15 @@
+package exams.exam2022q4.task02;
+
+public class Present {
+
+  private final double price;
+
+  public Present(double price) {
+    this.price = price;
+  }
+
+  public double price() {
+    return price;
+  }
+
+}
