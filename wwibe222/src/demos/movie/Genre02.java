2ad927ac5215274f0a95dcb180793491e204176c
D054906 <D054906@W-PF2XDCR9>
2023-04-03 12:04:58 +0200

    genre

--- wwibe222/src/demos/movie/Genre02.java
+++ wwibe222/src/demos/movie/Genre02.java
@@ -2,16 +2,21 @@ package demos.movie;
 
 public enum Genre02 {
 
-	HORROR("Horror"), ACTION("Action"), CRIME("Crime"), THRILLER("Thriller");
+  DRAMA("Drama"), ANIMATION("Animation"), CRIME("Crime"), FILM_NOIR("Film Noir"), FAMILY(
+      "Family"), ADVENTURE("Adventure"), MYSTERY("Mystery"), DOCUMENTARY("Documentary"), FANTASY(
+          "Fantasy"), HISTORY("History"), NEWS("News"), MUSIC("Music"), BIOGRAPHY(
+              "Biography"), COMEDY("Comedy"), SCIENCE_FICTION("Science Fiction"), SPORT(
+                  "Sport"), ROMANCE("Romance"), ACTION("Action"), THRILLER("Thriller"), HORROR(
+                      "Horror"), WESTERN("Western"), WAR("War"), MUSICAL("Musical");
 
-	private String name;
+  private String name;
 
-	Genre02(String name) {
-		this.name = name;
-	}
+  Genre02(String name) {
+    this.name = name;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
 }
