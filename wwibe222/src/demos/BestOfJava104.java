beb53ac20373ab810a2ee5e3b1e4568ce9d3ef93
D054906 <D054906@W-PF2XDCR9>
2023-04-03 14:45:52 +0200

    formatting

--- wwibe222/src/demos/BestOfJava104.java
+++ wwibe222/src/demos/BestOfJava104.java
@@ -1,36 +1,35 @@
 package demos;
 
 import java.util.ArrayList;
-
 import demos.movie.Genre02;
 import demos.movie.Movie03;
 
 /**
- * Aufzählungen (Enumerations)
+ * Aufzaehlungen (Enumerations)
  * 
  * @author Daniel Appenmaier
  *
  */
 public class BestOfJava104 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Movie03 movie1 = new Movie03("John Wick 4", new ArrayList<>(), "2023", 169, 8.4, 61087);
-		Movie03 movie2 = new Movie03("John Wick 4", new ArrayList<>(), "2023", 169, 8.4, 61087);
+    Movie03 movie1 = new Movie03("John Wick 4", new ArrayList<>(), "2023", 169, 8.4, 61087);
+    Movie03 movie2 = new Movie03("John Wick 4", new ArrayList<>(), "2023", 169, 8.4, 61087);
 
-		movie1.getGenres().add(Genre02.ACTION);
-		movie1.getGenres().add(Genre02.CRIME);
-		movie1.getGenres().add(Genre02.THRILLER);
-		movie2.getGenres().add(Genre02.ACTION);
-		movie2.getGenres().add(Genre02.CRIME);
-		movie2.getGenres().add(Genre02.THRILLER);
+    movie1.getGenres().add(Genre02.ACTION);
+    movie1.getGenres().add(Genre02.CRIME);
+    movie1.getGenres().add(Genre02.THRILLER);
+    movie2.getGenres().add(Genre02.ACTION);
+    movie2.getGenres().add(Genre02.CRIME);
+    movie2.getGenres().add(Genre02.THRILLER);
 
-		System.out.println(movie1.equals(movie2));
+    System.out.println(movie1.equals(movie2));
 
-		for (Genre02 g : Genre02.values()) {
-			System.out.println(g.getName());
-		}
+    for (Genre02 g : Genre02.values()) {
+      System.out.println(g.getName());
+    }
 
-	}
+  }
 
 }
--- wwibe222/src/demos/BestOfJava105.java
+++ wwibe222/src/demos/BestOfJava105.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import demos.movie.Genre02;
 import demos.movie.Movie03;
 
@@ -16,38 +15,38 @@ import demos.movie.Movie03;
  */
 public class BestOfJava105 {
 
-	public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException {
 
-		ArrayList<Movie03> movies = new ArrayList<>();
+    ArrayList<Movie03> movies = new ArrayList<>();
 
-		File file = new File("src/resources/movies.csv");
+    File file = new File("src/resources/movies.csv");
 
-		Scanner scanner = new Scanner(file);
+    Scanner scanner = new Scanner(file);
 
-		while (scanner.hasNextLine()) {
-			String line = scanner.nextLine();
-			String[] tokens = line.split(";");
-			String title = tokens[0];
-			double rating = Double.valueOf(tokens[1]);
-			int runtimeInMinutes = Integer.valueOf(tokens[2]);
-			String year = tokens[3];
-			ArrayList<Genre02> genres = new ArrayList<>();
-			String[] genreTexts = tokens[4].split(",");
-			for (String text : genreTexts) {
-				Genre02 genre = Genre02.valueOf(text);
-				genres.add(genre);
-			}
-			int votes = Integer.valueOf(tokens[5]);
-			Movie03 movie = new Movie03(title, genres, year, runtimeInMinutes, rating, votes);
-			movies.add(movie);
-		}
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] tokens = line.split(";");
+      String title = tokens[0];
+      double rating = Double.valueOf(tokens[1]);
+      int runtimeInMinutes = Integer.valueOf(tokens[2]);
+      String year = tokens[3];
+      ArrayList<Genre02> genres = new ArrayList<>();
+      String[] genreTexts = tokens[4].split(",");
+      for (String text : genreTexts) {
+        Genre02 genre = Genre02.valueOf(text);
+        genres.add(genre);
+      }
+      int votes = Integer.valueOf(tokens[5]);
+      Movie03 movie = new Movie03(title, genres, year, runtimeInMinutes, rating, votes);
+      movies.add(movie);
+    }
 
-		scanner.close();
+    scanner.close();
 
-		for (Movie03 m : movies) {
-			System.out.println(m);
-		}
+    for (Movie03 m : movies) {
+      System.out.println(m);
+    }
 
-	}
+  }
 
 }
